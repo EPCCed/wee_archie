371f7d88e2608b19dc85287d3b6cfdb3758af61a
Alistair Grant <alistair.grant@ed.ac.uk>
2016-07-07 13:30:01 +0100

    AG:updated to ignore pyc

--- framework/server/config.py
+++ framework/server/config.py
@@ -1,6 +1,8 @@
 import os
+
 DEBUG=True
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+RESULTS_DIR = os.path.join(BASE_DIR, 'results')
 DATABASE_CONNECTION = os.path.join(BASE_DIR, 'simulations.db')
 DB_CONNECTION_OPTS = {}
 STATUS_NAMES={'ready':'READY','running':'RUNNING','complete':'COMPLETE','error':'ERROR'}
